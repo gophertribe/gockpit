2a62d37a8d12bf757bf298a4a0145fd9f07f2e33
Michal Klimuk <michal@gophertribe.com>
2024-01-14 12:40:24 +0100

    refac: abstract hardware metrics

--- hardware/monitor.go
+++ hardware/monitor.go
@@ -2,8 +2,6 @@ package hardware
 
 import (
 	"context"
-	"fmt"
-	"github.com/nakabonne/tstorage"
 	"sync"
 	"time"
 
@@ -17,8 +15,6 @@ import (
 	"github.com/shirou/gopsutil/mem"
 )
 
-var _ MetricsProvider = &Monitor{}
-
 type Metrics struct {
 	Namespace string `json:"namespace"`
 }
@@ -31,45 +27,32 @@ type Logger interface {
 	Info(string)
 }
 
+type MetricsWriter interface {
+	HardwareStateUpdate(s State) error
+}
+
+type State struct {
+	MemTotal    uint64  `json:"mem_total"`
+	MemUsed     uint64  `json:"mem_used"`
+	MemPercent  float64 `json:"mem_percent"`
+	CpuPercent  float64 `json:"cpu_percent"`
+	DiskPercent float64 `json:"disk_percent"`
+	Uptime      uint64  `json:"uptime"`
+	Boottime    uint64  `json:"boottime"`
+}
+
 type Monitor struct {
 	mx      sync.Mutex
-	state   map[string]interface{}
+	state   State
 	logger  Logger
-	metrics tstorage.Storage
+	metrics MetricsWriter
 }
 
-func NewMonitor(metricsPath string, retention time.Duration, logger gockpit.Logger) (*Monitor, error) {
-	metrics, err := tstorage.NewStorage(tstorage.WithDataPath(metricsPath), tstorage.WithTimestampPrecision(tstorage.Milliseconds), tstorage.WithRetention(retention))
-	if err != nil {
-		return nil, fmt.Errorf("could not create hardware metrics storage: %w", err)
-	}
+func NewMonitor(mw MetricsWriter, logger gockpit.Logger) (*Monitor, error) {
 	return &Monitor{
-		state:   make(map[string]interface{}),
 		logger:  logger,
-		metrics: metrics,
+		metrics: mw,
 	}, nil
-}
-
-var metricNames = []string{"mem_total", "mem_used", "mem_percent", "cpu_percent", "disk_percent", "uptime", "boottime"}
-
-func (hw *Monitor) GetMetrics(from, to int64) (map[string][]*tstorage.DataPoint, error) {
-	res := map[string][]*tstorage.DataPoint{}
-	for _, m := range metricNames {
-		points, err := hw.GetMetric(m, nil, from, to)
-		if err != nil {
-			return nil, fmt.Errorf("could not get metric %s: %w", m, err)
-		}
-		res[m] = points
-	}
-	return res, nil
-}
-
-func (hw *Monitor) GetMetric(metric string, labels []tstorage.Label, from, to int64) ([]*tstorage.DataPoint, error) {
-	points, err := hw.metrics.Select(metric, labels, from, to)
-	if err != nil {
-		return nil, fmt.Errorf("could not select metric: %w", err)
-	}
-	return points, nil
 }
 
 func (hw *Monitor) Watch(ctx context.Context, pub Publisher, errs state.ErrorCollector, logger gockpit.Logger, wg *sync.WaitGroup) {
@@ -98,30 +81,20 @@ func (hw *Monitor) updateState(ctx context.Context, pub Publisher, errs state.Er
 	defer cancel()
 	memory, err := mem.VirtualMemoryWithContext(ctx)
 	_ = errs.Collect(ctx, "hw", "mem_read", "could not read memory stats", err, state.Clearable)
-	hw.state["mem_total"] = memory.Total
-	hw.state["mem_used"] = memory.Used
-	hw.state["mem_percent"] = memory.UsedPercent
+	hw.state.MemTotal = memory.Total
+	hw.state.MemUsed = memory.Used
+	hw.state.MemPercent = memory.UsedPercent
 	processor, err := cpu.PercentWithContext(ctx, 0, false)
 	_ = errs.Collect(ctx, "hw", "cpu_read", "could not read cpu stats", err, state.Clearable)
-	hw.state["cpu_percent"] = processor[0]
+	hw.state.CpuPercent = processor[0]
 	diskUsage, err := disk.UsageWithContext(ctx, "/")
 	_ = errs.Collect(ctx, "hw", "disk_read", "could not read disk usage stats", err, state.Clearable)
-	hw.state["disk_percent"] = diskUsage.UsedPercent
+	hw.state.DiskPercent = diskUsage.UsedPercent
 	info, err := host.InfoWithContext(ctx)
 	_ = errs.Collect(ctx, "hw", "host_read", "could not read hardware info", err, state.Clearable)
-	hw.state["uptime"] = info.Uptime
-	hw.state["boottime"] = info.BootTime
-	now := time.Now().UnixMilli()
-	err = hw.metrics.InsertRows(
-		[]tstorage.Row{
-			{Metric: "mem_total", DataPoint: tstorage.DataPoint{Timestamp: now, Value: float64(memory.Total)}},
-			{Metric: "mem_used", DataPoint: tstorage.DataPoint{Timestamp: now, Value: float64(memory.Used)}},
-			{Metric: "mem_percent", DataPoint: tstorage.DataPoint{Timestamp: now, Value: memory.UsedPercent}},
-			{Metric: "cpu_percent", DataPoint: tstorage.DataPoint{Timestamp: now, Value: processor[0]}},
-			{Metric: "disk_percent", DataPoint: tstorage.DataPoint{Timestamp: now, Value: diskUsage.UsedPercent}},
-			{Metric: "uptime", DataPoint: tstorage.DataPoint{Timestamp: now, Value: float64(info.Uptime)}},
-			{Metric: "boottime", DataPoint: tstorage.DataPoint{Timestamp: now, Value: float64(info.BootTime)}},
-		})
+	hw.state.Uptime = info.Uptime
+	hw.state.Boottime = info.BootTime
+	err = hw.metrics.HardwareStateUpdate(hw.state)
 	_ = errs.Collect(ctx, "hw", "metrics", "error publishing metrics", err, state.Clearable)
 	err = pub.Publish(ctx, gockpit.Event{
 		Namespace: "hw",
