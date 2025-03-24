0d50c2a12aff7eba108e76ad99dd9b33ad1e03d3
Michal Klimuk <michal@gophertribe.com>
2025-03-24 13:12:35 +0100

    chore: fix test

--- metrics/grafana/grafana_test.go
+++ metrics/grafana/grafana_test.go
@@ -1,12 +1,13 @@
 package grafana
 
 import (
+	"context"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
 func TestGrafana(t *testing.T) {
-	g := New("http://192.168.88.199:3000", []string{})
-	require.NoError(t, g.SetupInflux())
+	g := New("http://192.168.88.199:3000")
+	require.NoError(t, g.SetupInfluxDatasource(context.TODO(), "test", "test", "test", "test", "test", nil))
 }
